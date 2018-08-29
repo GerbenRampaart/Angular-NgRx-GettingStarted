d8301afaddd74fde386bc9341d9fcf77e0d75e75
Gerben Rampaart <rampaart@live.com>
2018-08-29 09:01:22 +0200

    Course ongoing

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,11 +1,54 @@
-export function reducer(state, action) {
-    switch (action.type) {
-        case 'TOGGLE_PRODUCT_CODE':
-            return {
-                ...state, showProductCode: action.payload
-            };
-            break;
-        default:
-            return state;
-    }
+import { Product } from '../product';
+import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { ProductActions, ProductActionTypes } from './product.actions';
+
+const initialState: ProductState = {
+    showProductCode: true,
+    currentProduct: null,
+    products: []
+};
+
+export interface State extends fromRoot.State {
+    products: ProductState;
+}
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  product => product.currentProduct
+);
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+export function reducer(state = initialState, action: ProductActions): ProductState {
+  switch (action.type) {
+    case ProductActionTypes.ToggleProductCode:
+      return {
+        ...state,
+        showProductCode: action.payload
+      };
+    case ProductActionTypes.SetCurrentProduct:
+      return {
+        ...state,
+        currentProduct: { ...action.payload } // 'current' product is a copy of the product
+      };
+    case ProductActionTypes.ClearCurrentProduct:
+      return {
+        ...state,
+        currentProduct: null
+      };
+    default:
+      return state;
+  }
 }
